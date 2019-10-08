1cebb40abcc2371aed190f4894be7b26899bd4fe
Dennis Padiernos <depadiernos@gmail.com>
2019-10-08 17:19:30 -0500

    forgot to export the setter.

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 
-export const useDarkMode = () => {
-  const [isDarkMode, setIsDarkMode] = useLocalStorage('isDarkMode', false);
+export const useDarkMode = (initialValue) => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorage('isDarkMode', initialValue);
   useEffect(()=>{
     const body = document.querySelector('body');
     isDarkMode ? body.classList.add('dark-mode'): body.classList.remove('dark-mode');
